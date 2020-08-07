81128e03a4b30a63dfeae3fddc157f5446ffad8a
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-08-07 10:17:58 -0500

    oop1 done

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -21,3 +21,31 @@
 '''
 Vehicle is the base class.
 '''
+
+
+class Vehicle:
+    pass
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
